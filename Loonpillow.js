3c1d58db1b69b86268fb56343885eb37419f9a13
Fromboy <93315528+Fromboy@users.noreply.github.com>
2025-09-19 10:01:06 +0800

    Create Loonpillow.js

---
+++ Loonpillow.js
@@ -0,0 +1,47 @@
+// 检查 $response 是否存在
+if (typeof $response === 'undefined' || !$response.body) {
+  console.log('Error: $response or $response.body is undefined');
+  $done({});
+  return;
+}
+
+// 定义新的 JSON 对象
+let obj = {
+  "request_date": "2022-08-06T02:30:14Z",
+  "request_date_ms": 1837536263000,
+  "subscriber": {
+    "entitlements": {
+      "premium": {
+        "expires_date": "2023-08-06T02:30:14Z",
+        "grace_period_expires_date": null,
+        "product_identifier": "com.neybox.pillow.premium.month",
+        "purchase_date": "2022-08-06T02:30:14Z"
+      }
+    },
+    "first_seen": "2022-08-06T02:30:14Z",
+    "last_seen": "2022-08-06T02:30:14Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "YOUR_USER_ID",
+    "original_application_version": "YOUR_APP_VERSION",
+    "original_purchase_date": "2022-08-06T02:30:14Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "com.neybox.pillow.premium.month": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2999-08-06T02:30:14Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2022-08-06T02:30:14Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "active",
+        "purchase_date": "2022-08-06T02:30:14Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": "2022-09-08T02:30:14Z"
+      }
+    }
+  }
+};
+
+// 返回修改后的响应
+$done({ body: JSON.stringify(obj) });
